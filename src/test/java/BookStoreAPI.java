52560d1567efef482c073c5e80aa934ecc611c83
dinosaur321 <dinosaur321@users.com>
2021-10-28 17:04:35 +0300

    Add map to manipulate user fields

--- src/test/java/BookStoreAPI.java
+++ src/test/java/BookStoreAPI.java
@@ -10,6 +10,10 @@ import org.hamcrest.Matchers;
 import org.json.JSONObject;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
@@ -18,8 +22,8 @@ import static org.hamcrest.collection.IsMapContaining.hasKey;
 
 public class BookStoreAPI {
 
-    User user = new User();
-    AuthToken authToken = new AuthToken();
+    static HashMap<String,String> userMap = new HashMap<>();
+
 
     @Test
     public void checkRestAssured() {
@@ -30,85 +34,88 @@ public class BookStoreAPI {
                 .assertThat().header("Content-Type", "text/html; charset=UTF-8");
     }
 
-    @Test
-    public void createUserSuccess() {
-        //  JSONObject requestParams = new JSONObject();
-//        requestParams.put("password", "Password!23");
-//        requestParams.put("userName", "userName2");
-        RequestSpecification body = getPreparedRequest()
-                .header("accept", "application/json")
-                .header("Content-Type", "application/json")
-                .body("{ \"userName\": \"userName13\", \"password\": \"Password!23\"}");
-        ValidatableResponse response = body.when().post("Account/v1/User").then().assertThat().statusCode(406)
-                .log().all();
+//    @Test
+//    public void createUserSuccess() {
+//        RequestSpecification body = getPreparedRequest()
+//                .header("accept", "application/json")
+//                .header("Content-Type", "application/json")
+//                .body("{ \"userName\": \"userName13\", \"password\": \"Password!23\"}");
+//        ValidatableResponse response = body.when().post("Account/v1/User").then().assertThat().statusCode(406)
+//                .log().all();
+//
+////        getPreparedRequest()
+////                .header("accept: application/json","Content-Type: application/json")
+////                .body("{ \"userName\": \"userName2\", \"password\": \"Password!23\"}")
+////                .when().post("/swagger/#/Account/AccountV1UserPost")
+////                .then().assertThat().statusCode(200).assertThat().body(Matchers.notNullValue());
+//    }
 
-//        getPreparedRequest()
-//                .header("accept: application/json","Content-Type: application/json")
-//                .body("{ \"userName\": \"userName2\", \"password\": \"Password!23\"}")
-//                .when().post("/swagger/#/Account/AccountV1UserPost")
-//                .then().assertThat().statusCode(200).assertThat().body(Matchers.notNullValue());
-    }
-
-    @Test
+    @Test (priority = 2)
     public void checkUserExists() {
         getPreparedRequest()
                 .header("accept", "application/json")
                 .header("Content-Type", "application/json")
-                .body("{ \"userName\": \"userName8\", \"password\": \"Password!23\"}")
+                .body("{ \"userName\": \""+ userMap.get("username")+"\", \"password\": \"Password!23\"}")
                 .when().post("Account/v1/User").then()
                 .assertThat().statusCode(406)
                 .assertThat().body("code", equalTo("1204"))
                 .assertThat().body("message", equalTo("User exists!")).log().all();
     }
 
-    @Test
+    @Test (priority = 1)
     public void createNewUser() {
+
+        String userName = givenRandomString();
+        userMap.put("username", userName);
+        userMap.put("password","Password!23");
         ValidatableResponse response = getPreparedRequest()
                 .header("accept", "application/json")
                 .header("Content-Type", "application/json")
-                .body("{ \"userName\": \"userName12\", \"password\": \"Password!23\"}")
+                .body("{ \"userName\": \""+ userMap.get("username")+"\", \"password\": \"Password!23\"}")
                 .when().post("Account/v1/User").then()
                 .assertThat().statusCode(201)
-                .assertThat().body("username", equalTo("userName12")).log().all();
+                .assertThat().body("username", equalTo(userName)).log().all();
 
-        user.setUserId(response.extract().path("userID"));
+
+        userMap.put("UUID", response.extract().path("userID"));
+
+               // setUserId(response.extract().path("userID"));
 //        System.out.println(user.getUserId());
     }
 
-    @Test
-    public void checkUserAuthorizedSuccess() {
+
+    @Test(priority = 5)
+    public void checkGetUserAuthorized() {
         ValidatableResponse response = getPreparedRequest()
                 .header("accept", "application/json")
                 .header("Content-Type", "application/json")
-                .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6InVzZXJOYW1lMTMiLCJwYXNzd29yZCI6IlBhc3N3b3JkITIzIiwiaWF0IjoxNjM1NDE5MTk5fQ.n1us2Bz_gNBb15go4E8AoSVzhjqQd_ZfM55Dt7C0uCU")
-                .pathParam("UUID", "cfd71945-c131-467c-a368-ef4c7796afb8")
+                .header("Authorization", "Bearer "+ userMap.get("token"))
+                .pathParam("UUID", userMap.get("UUID"))
                 .when().get("Account/v1/User/{UUID}")
                 .then().assertThat().statusCode(200).log().all();
     }
 
-    @Test
+    @Test(priority = 3)
     public void generateTokenSuccess() {
         ValidatableResponse response = getPreparedRequest()
                 .header("accept", "application/json")
                 .header("Content-Type", "application/json")
-                .body("{ \"userName\": \"userName12\", \"password\": \"Password!23\"}")
+                .body("{ \"userName\": \""+ userMap.get("username")+"\", \"password\": \"Password!23\"}")
                 .when().post("Account/v1/GenerateToken")
                 .then().assertThat().statusCode(200)
                 .assertThat().body("status", equalTo("Success")).log().all();
-        authToken.setToken(response.extract().path("token"));
+        userMap.put("token",(response.extract().path("token")));
     }
 
-    @Test
-    public void AuthorizedSuccess() {
+    @Test (priority = 4)
+    public void checkAuthorizedSuccess() {
         ValidatableResponse response = getPreparedRequest()
                 .header("accept", "application/json")
                 .header("Content-Type", "application/json")
-                .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6InVzZXJOYW1lMTMiLCJwYXNzd29yZCI6IlBhc3N3b3JkITIzIiwiaWF0IjoxNjM1NDE5MTk5fQ.n1us2Bz_gNBb15go4E8AoSVzhjqQd_ZfM55Dt7C0uCU")
-                .body("{ \"userName\": \"userName13\", \"password\": \"Password!23\"}")
+                .header("Authorization", "Bearer "+ userMap.get("token"))
+                .body("{ \"userName\": \""+ userMap.get("username")+"\", \"password\": \"Password!23\"}")
                 .when().post("/Account/v1/Authorized")
                 .then().assertThat().statusCode(200).log().all();
-//                .assertThat().body("", equalTo(true)).log().all();
-//        authToken.setToken(response.extract().path("token"));
     }
 
 
@@ -122,6 +129,23 @@ public class BookStoreAPI {
 
     public static RequestSpecification getPreparedRequest() {
         return given().spec(getRequestSpec()).log().all();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(userMap);
+    }
+
+    public static String givenRandomString() {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 5;
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+        return generatedString;
     }
 
 }
